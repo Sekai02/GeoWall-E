0077dc0c1e4314be4897bb41e8e82623239dc493
Jossue <jossuearteche000@gmail.com>
2023-11-28 14:33:13 -0500

    Class for grouping expressions added

---
+++ GeoWallECompiler/Expressions/Grouping.cs
@@ -0,0 +1,11 @@
+﻿namespace GeoWallECompiler.Expressions;
+public class Grouping : GSharpExpression
+{
+    public Grouping(GSharpExpression expression)
+    {
+        Expression = expression;
+    }
+    public GSharpExpression Expression { get; }
+
+    public override T Accept<T>(IExpressionVisitor<T> visitor) => Expression.Accept(visitor);
+}
