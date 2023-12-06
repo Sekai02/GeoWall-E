dfe0e5d58cd485c99c7f9558b2ca5ad80219d737
Sekai02 <luisalejandroarteagamorales@gmail.com>
2023-12-05 21:55:56 -0500

    import handler

---
+++ GeoWallECompiler/Compiler/ImportHandler.cs
@@ -0,0 +1,26 @@
+using GeoWallECompiler.Visitors;
+
+namespace GeoWallECompiler;
+
+public class Container
+{
+    public Context<GSObject?, ICallable> Environment;
+    public Context<GSharpType, ICallable> TypeEnvironment;
+
+    public Container(Context<GSObject?, ICallable> environment, Context<GSharpType, ICallable> typeEnvironment)
+    {
+        Environment = environment;
+        TypeEnvironment = typeEnvironment;
+    }
+}
+
+public static class ImportHandler
+{
+    private static Container GetEnvironments(string source, IDrawer drawer, IWalleUI userInterface) => GSharp.RunLibrary(source, drawer, userInterface);
+
+    //El problema es aqui Jossue
+    private static void SaveLibrary(string source)
+    {
+        Container environments = GetEnvironments(source, new DrawerDummie(), new ConsoleUI())
+    }
+}
