5e629ad5991f75340f18cbaa2eba362c621c38f1
Jossue <jossuearteche000@gmail.com>
2023-12-13 21:57:41 -0500

    Fixed Point intersection

--- GeoWallECompiler/StandardLibrary/SequenceFunctions.cs
+++ GeoWallECompiler/StandardLibrary/SequenceFunctions.cs
@@ -61,22 +61,45 @@ public class IntersectFunction : ICallable
     public GSObject? Evaluate(Evaluator evaluator, List<GSObject?> arguments)
     {
         if (arguments.Count != 2)
-            throw new SemanticError("Function 'intersec'", "2 arguments", $"{arguments.Count}");
+            throw new SemanticError("Function 'intersect'", "2 arguments", $"{arguments.Count}");
         GSObject? argument1 = arguments[0];
         GSObject? argument2 = arguments[1];
         if (argument1 is null || argument2 is null)
             throw new DefaultError("Function 'intersect' cannot take an undefined argument");
         if (argument1 is IDrawable figure1 && argument2 is IDrawable figure2)
         {
+            if(argument1 is GSPoint point1 && argument2 is GSPoint point2)
+            {
+                List<GSPoint> result = new();
+                if (point1.Coordinates.X == point2.Coordinates.X && point1.Coordinates.Y == point2.Coordinates.Y)
+                    result.Add(point1);
+                return new GSequence(result, result.Count);
+            }
+            if (argument1 is GSPoint p1)
+            {
+                List<GSPoint> result = new();
+                Equation equation = figure2.Equation;
+                if (EquationSolver.EvaluateEcuation(p1.Coordinates.X, p1.Coordinates.Y, equation) == 0)
+                    result.Add(p1);
+                return new GSequence(result, result.Count);
+            }
+            if (argument2 is GSPoint p2)
+            {
+                List<GSPoint> result = new();
+                Equation equation = figure1.Equation;
+                if (EquationSolver.EvaluateEcuation(p2.Coordinates.X, p2.Coordinates.Y, equation) == 0)
+                    result.Add(p2);
+                return new GSequence(result, result.Count);
+            }
             Equation equation1 = figure1.Equation;
             Equation equation2 = figure2.Equation;
             List<GSPoint> points = new(EquationSolver.SolveCircularSystem(equation1, equation2));
             return new GSequence(points, points.Count);            
         }
-        if (argument1 is not GSPoint)
-            throw new SemanticError("Function 'Segment'", "point", argument1.GetType().Name);
+        if (argument1 is not IDrawable)
+            throw new SemanticError("Function 'intersect'", "figure", argument1.GetType().Name);
         else
-            throw new SemanticError("Function 'Segment'", "point", argument2.GetType().Name);
+            throw new SemanticError("Function 'intersect'", "figure", argument2.GetType().Name);
     }
     public int GetArgumentsAmount() => 2;
     public GSharpType GetType(TypeChecker checker, List<GSharpType> argumentsTypes) => new(GTypeNames.GSequence, GTypeNames.Point);
